8afd2c5512ade8c288638cf59263ddbb99ddce64
zach96guan <zehaog@cs.cmu.edu>
2019-06-23 18:23:25 -0700

    updated

---
+++ commands.sh
@@ -0,0 +1,25 @@
+git remote -v
+git remote add origin <url>
+git push -u origin master
+
+git log --oneline --graph
+git show head
+git show master~
+git show master~2
+git show master^
+git show master^^
+git show master^2
+git show master~^2
+
+git branch
+git checkout master
+git checkout -b featureX
+git branch -d featureX
+git branch -D featureX
+git reflog
+
+git merge featureX
+git merge --no-ff featureX
+
+
+
