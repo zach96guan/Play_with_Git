16d79ea9e0d0be22243cff0cc3faa8a8f7fa6a86
zach96guan <zehaog@cs.cmu.edu>
2019-06-24 19:44:34 -0700

    update cmds

--- commands.sh
+++ commands.sh
@@ -1,8 +1,9 @@
 git remote -v
 git remote add origin <url>
-git push -u origin master
+git push -u origin master  # (--set-upstream)
 
 git log --oneline --graph
+
 git show head
 git show master~
 git show master~2
@@ -21,5 +22,25 @@ git reflog
 git merge featureX
 git merge --no-ff featureX
 
+git branch --all
+git log origin/master --oneline
+git remote set-head <remote> <branch>
 
+git fetch
+git pull  # git fetch + git merge FETCH_HEAD
+git pull --ff-only
+git pull --no-ff
+
+git checkout featureX
+git rebase master
+git rebase master featureX  # git rebase <upstream> <branch>
+
+git rebase --continue
+git rebase --abort
+
+git commit --amend -m <msg>
+git commit --amend --no-edit
+git rebase -i <after-this-commit>  # interactive rebase
+
+git merge --squash featureX
 
